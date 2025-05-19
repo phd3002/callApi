b00ab79aad213db3d8922b8faa0e5979aafc196d
Phan Hung <hungdphe170721@fpt.edu.vn>
2025-05-19 15:07:22 +0700

    Initial commit

---
+++ src/main/java/com/post/callApi/CallApiApplication.java
@@ -0,0 +1,13 @@
+package com.post.callApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/post/callApi/CallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.callApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
